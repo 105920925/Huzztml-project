82c2d32c04a859c7ab0c0c570cdf077018afb40b
105920925 <105920925@student.swin.edu.au>
2025-05-26 11:12:47 +1000

    fixed login

--- project2/db/Settings.php
+++ project2/db/Settings.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = "";
 $dbname = "jobs";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname); // This connects to the 'jobs' database
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
